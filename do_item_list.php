cef371d8f9ce2cb6083fe07498ba9372e3930eb7
Masahiro <masahiro.infinite77@gmail.com>
2018-04-14 14:13:02 +0900

    ピザの数量変更処理を修正

--- do_item_list.php
+++ do_item_list.php
@@ -9,34 +9,34 @@ if(strcmp($user, '') == 0) {
 
 if($_POST['itemA'] == 'checked') {
     $_SESSION['itemA']++;
+}else {
+    $_SESSION['itemA']+=0;
 }
-else {
-    $_SESSION['itemA'] = 0;
-}
+
 if($_POST['itemB'] == 'checked') {
     $_SESSION['itemB']++;
+}else {
+    $_SESSION['itemB']+=0;
 }
-else {
-    $_SESSION['itemB'] = 0;
-}
+
 if($_POST['itemC'] == 'checked') {
     $_SESSION['itemC']++;
+}else {
+    $_SESSION['itemC']+=0;
 }
-else {
-    $_SESSION['itemC'] = 0;
-}
+
 if($_POST['itemD'] == 'checked') {
     $_SESSION['itemD']++;
+}else {
+    $_SESSION['itemD']+=0;
 }
-else {
-    $_SESSION['itemD'] = 0;
-}
+
 if($_POST['itemE'] == 'checked') {
     $_SESSION['itemE']++;
+}else {
+    $_SESSION['itemE']+=0;
 }
-else {
-    $_SESSION['itemE'] = 0;
-}
+
 
 header('Location: item_list.php');
 ?>
